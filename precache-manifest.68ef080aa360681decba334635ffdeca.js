4365bc0b928954023fae47ddf66bed79b5459f3c
An Nguyen <tranminhannguyen@gmail.com>
2019-09-25 01:29:20 +0700

    Updates

---
+++ precache-manifest.68ef080aa360681decba334635ffdeca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18083bd89b3b259e4f37408e8b82f1f6",
+    "url": "/CaroReactJSUpdated/index.html"
+  },
+  {
+    "revision": "f83d9975f7b8a9615efb",
+    "url": "/CaroReactJSUpdated/static/css/main.b352e498.chunk.css"
+  },
+  {
+    "revision": "c7d936580784bca419a2",
+    "url": "/CaroReactJSUpdated/static/js/2.f0b76aab.chunk.js"
+  },
+  {
+    "revision": "f83d9975f7b8a9615efb",
+    "url": "/CaroReactJSUpdated/static/js/main.641c451b.chunk.js"
+  },
+  {
+    "revision": "3b54472528c397fd6ba3",
+    "url": "/CaroReactJSUpdated/static/js/runtime-main.906ae283.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CaroReactJSUpdated/static/media/logo.5d5d9eef.svg"
+  }
+]);
